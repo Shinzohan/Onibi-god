baf93c529e6d74022ca87da8ee544f7e126d094c
shinzo <pikachupokemon004@gmail.com>
2024-07-09 20:58:35 +0530

    metatag change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Onibi",
-  description: "Onibi",
+  description: "Aniflow Interactive is the developer for Onibi, an upcoming adventure and exploration game. Dash, possess, and explore your way through different areas as you accompany a child on their adventures as their fledgling guardian spirit",
 };
 
 export default function RootLayout({
